f6df77e0f4065185dea6312c968932285a6fb7f1
Jane <spring1981@mail.ru>
2024-02-14 19:16:20 +0300

    repository was created

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('src/public'));
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+http.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
